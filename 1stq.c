37010c07b65a518596e1a3b4127e69941b74323f
shiv123jnjnf <prajaktapujari93@gmail.com>
2024-03-08 16:50:54 +0530

    first commit

---
+++ 1stq.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum,diff,multi;
+printf("enter two numbers a and b\n");
+scanf("%d %d",&a,&b);
+ 
+ printf("Sum=%d\n",sum=a+b);
+
+printf("Difference=%d\n",diff=a-b);
+
+printf("Multiplication=%d\n",multi=a*b);
+return 0;
+}
